94857f0508df264f8d31e6a9e26532cfad98bbb7
paulevsGitch <paulevs@yandex.ru>
2026-04-26 23:32:15 +0300

    Item usage, item animations

--- api/api_item.lua
+++ api/api_item.lua
@@ -1,9 +1,14 @@
 ---@meta
 
+---@class OnUseDef
+---@field primary_use? fun(player: Player) Function that will be called on primary use of item (left click by default), optional
+---@field secondary_use? fun(player: Player) Function that will be called on secondary use of item (right click by default), optional
+
 ---@class ItemDef
 ---@field mesh? string Item mesh, optional
 ---@field view_mode? "side"|"top" Icon view mode, optional. Default is "side"
 ---@field tags? string[] Array of tags, each tag is identifier
+---@field on_use? OnUseDef
 
 ---Register new Item with specidied identifier and definition.
 ---@param identifier string Item identifier
--- api/api_player.lua
+++ api/api_player.lua
@@ -9,3 +9,12 @@ Player = {}
 ---@param count? integer Item count, optional. Default is 1
 ---@return integer result Remained item count
 function Player:add_item(item_id, count) end
+
+---@class ItemAnimationKey
+---@field length number Animation key length in seconds
+---@field position? Vector3 Position in relative space, optional. If not specified position will not change
+---@field rotation? Vector3 Rotation in relative space, optional. If not specified position will not change
+
+---Plays animation of item selected in player hotbar
+---@param animation_def ItemAnimationKey[]
+function Player:play_item_animation(animation_def) end
