acb15723a13c13827e471673c864b823151bf553
paulevsGitch <paulevs@yandex.ru>
2026-04-28 15:19:40 +0300

    Item usage time

--- api/api_item.lua
+++ api/api_item.lua
@@ -1,8 +1,10 @@
 ---@meta
 
 ---@class OnUseDef
----@field primary_use? fun(player: Player) Function that will be called on primary use of item (left click by default), optional. Will be called only if current player item animation is not active
----@field secondary_use? fun(player: Player) Function that will be called on secondary use of item (right click by default), optional. Will be called only if current player item animation is not active
+---@field primary_use? fun(player: Player) Function that will be called on primary use of item (left click by default), optional
+---@field primary_use_length? number Time limit when the next primary usage of item can be called, optional
+---@field secondary_use? fun(player: Player) Function that will be called on secondary use of item (right click by default), optional
+---@field secondary_use_length? number Time limit when the next secondary usage of item can be called, optional
 
 ---@class ItemDef
 ---@field max_count? integer Max item stack size, optional. Should be in range [1-100], default is 100
