52c9c27f880c32654d7f2b2f8f1fb833d7bbbee9
paulevsGitch <paulevs@yandex.ru>
2026-04-28 16:11:43 +0300

    Timers API

--- api/api_time.lua
+++ api/api_time.lua
@@ -15,3 +15,14 @@ Time.get_microseconds = function () end
 ---Get time in seconds since the game start. Each world has its own starting time
 ---@return number
 Time.get_seconds = function () end
+
+---Calls function with a delay
+---@param action fun() Function that will be called after delay will be ended
+---@param start_delay number Delay in seconds before function will be called
+Time.call_with_delay = function (action, start_delay) end
+
+---Calls function with a specified period after specified delay
+---@param action fun(): boolean Function that will be called after delay will be ended. If function will return `true` the timer will stop
+---@param period number Delay in seconds between function calls
+---@param start_delay? number Delay in seconds before time will start
+Time.make_timer = function (action, period, start_delay) end
