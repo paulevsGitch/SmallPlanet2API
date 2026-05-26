b12af30ecab84c3ef9428cc6f911493fa3a5dedf
paulevsGitch <paulevs@yandex.ru>
2026-05-26 19:49:34 +0300

    Typo fix

--- api/api_terrain_material.lua
+++ api/api_terrain_material.lua
@@ -15,4 +15,4 @@
 ---@field color string|{ dark_color: string, bright_color: string, color_mixer: number|FloatFunction } Terrain color, in Hex RGBA format
 ---@field smooth_blending? boolean Smooth blending, will mix material color with neighbours that have smooth blending, optional. Default is false
 ---@field decorations? TerrainDecorationDefinition[] Array of terrain decoration definitions
----@field sounds? {footstep?: RandomSoundsDef} Table with sound definitions
+---@field sounds? {footstep?: RandomSoundsDefinition} Table with sound definitions
