52ff7481b3a19b4b1c50b7595f82b9145621e9f2
paulevsGitch <paulevs@yandex.ru>
2026-06-05 04:40:14 +0300

    Proper hit sound definition

--- api/tools_api.lua
+++ api/tools_api.lua
@@ -6,16 +6,16 @@ Tools = {}
 ---@class GameObjectToolData Drop definition used by Tools API
 ---@field max_hits number|fun(game_object: GameObject): number Maximum amount of tool hits that object can handle before breaking
 ---@field tool_tag Identifier Tool tag ID that can destroy this object
----@field break_sound? Identifier Breaking sound, this field is used by Tools API
+---@field hit_sound? Identifier Hit sound, played on each tool hit
+---@field break_sound? Identifier Breaking sound, played when onject is broken
 ---@field drop_item? ItemStack Item that will be dropped on each position
 ---@field drop_position? Vector3|Vector3[]|fun(game_object: GameObject): Vector3[] Drop positions relative to object
 
 ---Register new tool with default functionality and tags
 ---@param tool_id Identifier Tool Identifier
----@param hit_sound Identifier Tool hit sound Identifier
 ---@param tags table<string,integer> Item tags, should include tool type tag (for example "default:axe" for axes)
 ---@param mesh_id? Identifier Item model identifier, optional. If not provided it will be constructed based on tool Identifier
-function Tools.register_tool(tool_id, hit_sound, tags, mesh_id) end
+function Tools.register_tool(tool_id, tags, mesh_id) end
 
 ---Helper function to register a new axe tool with default functionality and tags
 ---@param tool_id Identifier Tool identifier
