46dcf66b147526327168d5649e7b25df6b44c88b
paulevsGitch <paulevs@yandex.ru>
2026-06-28 15:22:50 +0300

    Dynamic biome ambience

--- api/biome_api.lua
+++ api/biome_api.lua
@@ -12,9 +12,16 @@
 ---@field name Identifier TerrainMaterial Indentifier
 ---@field rule? TerrainCondition Material generation rule, optional
 
----@class GameBiomeDefinition
+---@class DynamicAmbientSoundDefinition Will place random sound emitters to create dynamic ambient sound
+---@field sound Identifier|RandomSoundDefinition Sound to play
+---@field min_distance number Minimal distance for the random sound placement
+---@field max_distance number Maximal distance for the random sound placement
+---@field min_delay number Minimal delay between sound emitters placement, in seconds
+---@field max_delay number Maximal delay between sound emitters placement, in seconds
+
+---@class GameBiomeDefinition Definition for the game biome
 ---@field terrain Identifier|GameBiomeTerrainDefinition[] Terrain Identifier or array of terrains with conditions
----@field ambient_sound? Identifier Biome ambient sound loop, optional
+---@field ambient_sound? Identifier|DynamicAmbientSoundDefinition[] Biome ambient sound loop, optional
 ---@field height? number|FloatFunction Height value or function for terrain generation, optional
 ---@field near_decorations? BiomeDecorationDefinition[] Near decorations (small objects), optional
 ---@field far_decorations? BiomeDecorationDefinition[] Far decorations (big objects), optional
